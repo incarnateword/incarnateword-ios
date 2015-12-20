7e818d7cdc34dbcbb4420ae7afa96990ef2023c8
AdityaDeshmane <aditya.deshmane@gmail.com>
2015-12-20 18:03:20 +0530

    Updated Chapter structure for serialization

--- Source/The Incarnate Word/The Incarnate Word/DataStructures/Chapter/IWDetailChapterStructure.h
+++ Source/The Incarnate Word/The Incarnate Word/DataStructures/Chapter/IWDetailChapterStructure.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface IWDetailChapterStructure : NSObject
+@interface IWDetailChapterStructure : NSObject<NSCoding>
 
 /*
  {
--- Source/The Incarnate Word/The Incarnate Word/DataStructures/Chapter/IWDetailChapterStructure.m
+++ Source/The Incarnate Word/The Incarnate Word/DataStructures/Chapter/IWDetailChapterStructure.m
@@ -10,4 +10,37 @@
 
 @implementation IWDetailChapterStructure
 
+-(void)encodeWithCoder:(NSCoder *)encoder
+{
+    [encoder encodeObject:_strDescription forKey:@"strDescription"];
+    [encoder encodeObject:_strDate forKey:@"strDate"];
+    [encoder encodeObject:_strNextChapter forKey:@"strNextChapter"];
+    [encoder encodeObject:_strNextChapterUrl forKey:@"strNextChapterUrl"];
+    [encoder encodeObject:_strPrevChapter forKey:@"strPrevChapter"];
+    [encoder encodeObject:_strPrevChapterUrl forKey:@"strPrevChapterUrl"];
+//    [encoder encodeObject:_arrPath forKey:@"arrPath"];
+    [encoder encodeObject:_strTitle forKey:@"strTitle"];
+    [encoder encodeObject:_strUrl forKey:@"strUrl"];
+    [encoder encodeObject:_strText forKey:@"strText"];
+}
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+    if (self = [super init])
+    {
+        _strDescription = [decoder decodeObjectForKey:@"strDescription"];
+        _strDate = [decoder decodeObjectForKey:@"strDate"];
+        _strNextChapter = [decoder decodeObjectForKey:@"strNextChapter"];
+        _strNextChapterUrl = [decoder decodeObjectForKey:@"strNextChapterUrl"];
+        _strPrevChapter = [decoder decodeObjectForKey:@"strPrevChapter"];
+        _strPrevChapterUrl = [decoder decodeObjectForKey:@"strPrevChapterUrl"];
+//        _arrPath = [decoder decodeObjectForKey:@"arrPath"];
+        _strTitle = [decoder decodeObjectForKey:@"strTitle"];
+        _strUrl = [decoder decodeObjectForKey:@"strUrl"];
+        _strText = [decoder decodeObjectForKey:@"strText"];
+    }
+    
+    return self;
+}
+
 @end
