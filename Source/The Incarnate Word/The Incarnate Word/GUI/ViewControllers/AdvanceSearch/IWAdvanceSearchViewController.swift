6b4fb196f1c0a56a9204c9917533ef85bdc50a6c
AdityaDeshmane <aditya.deshmane@gmail.com>
2016-06-24 00:31:01 +0530

    WIP: Advance Search UI

---
+++ Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/AdvanceSearch/IWAdvanceSearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  IWAdvanceSearchViewController.swift
+//  The Incarnate Word
+//
+//  Created by Aditya Deshmane on 24/06/16.
+//  Copyright © 2016 Revealing Hour Creations. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IWAdvanceSearchViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+}
