c02f36a0ae9f833cf6abaebf39a8dce1c15a89f4
AdityaDeshmane <aditya.deshmane@gmail.com>
2016-06-27 01:16:43 +0530

    WIP: Advance Search: Added backend controller files

---
+++ Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/AdvanceSearch/IWAdvanceSearchContainerTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  IWAdvanceSearchContainerTableViewController.swift
+//  The Incarnate Word
+//
+//  Created by Aditya Deshmane on 27/06/16.
+//  Copyright © 2016 Revealing Hour Creations. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IWAdvanceSearchContainerTableViewController: UITableViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+}
