646c660660780f87198c365589320e2e4070b81c
Aditya Deshmane <aditya.deshmane@gmail.com>
2016-07-25 12:37:38 +0530

    test commit

--- Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/AdvanceSearch/IWSelectionViewController.swift
+++ Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/AdvanceSearch/IWSelectionViewController.swift
@@ -111,6 +111,7 @@ class IWSelectionViewController: UIViewController,UITableViewDataSource,UITableV
         return cell
     }
     
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
     {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
