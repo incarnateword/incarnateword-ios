fd682c05001c82efb9fc72a8bcec4fb9387ecb33
AdityaDeshmane <aditya.deshmane@gmail.com>
2016-11-03 23:25:58 +0530

    Advance Search: Added empty more view and controller

---
+++ Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/AdvanceSearch/IWMoreViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IWMoreViewController.swift
+//  The Incarnate Word
+//
+//  Created by Aditya Deshmane on 03/11/16.
+//  Copyright © 2016 Revealing Hour Creations. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IWMoreViewController: UIViewController
+{
+    @IBOutlet weak var labelInfo: UILabel!
+
+}
