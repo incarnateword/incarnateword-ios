7e576fb4a62443cc9c4f20ce6c4b23db9816f6dd
AdityaDeshmane <aditya.deshmane@gmail.com>
2016-12-13 00:06:55 +0530

    Manage Notification: Updated time picker

--- Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/Notification/IWManageNotificationViewController.swift
+++ Source/The Incarnate Word/The Incarnate Word/GUI/ViewControllers/Notification/IWManageNotificationViewController.swift
@@ -12,11 +12,14 @@ import UIKit
 
 class IWManageNotificationViewController: UITableViewController
 {
-
+    var bIsFromCellExpanded:Bool = false
+    var bIsToCellExpanded:Bool = false
+    
     @IBOutlet weak var switchNotification: UISwitch!
     @IBOutlet weak var labelNotificationsPerDay: UILabel!
     @IBOutlet weak var cellTimeFrom: UITableViewCell!
-    @IBOutlet weak var cellTimeTo: UITableViewCell!
+    @IBOutlet weak var datePickerFrom: UIDatePicker!
+    @IBOutlet weak var datePickerTo: UIDatePicker!
     
     @IBAction func notificationPerDayStepperValueChanged(sender: AnyObject)
     {
@@ -25,4 +28,59 @@ class IWManageNotificationViewController: UITableViewController
     @IBAction func notitifacationSwitchValueChanged(sender: AnyObject)
     {
     }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        var rowHeight: CGFloat = 44
+        
+        if indexPath.section == 1 && indexPath.row == 0
+        {
+            datePickerFrom.hidden = !bIsFromCellExpanded
+            
+            if bIsFromCellExpanded
+            {
+                rowHeight = 200
+            }
+        }
+        else if indexPath.section == 1 && indexPath.row == 1
+        {
+            datePickerTo.hidden = !bIsToCellExpanded
+            
+            if bIsToCellExpanded
+            {
+                rowHeight = 200
+            }
+        }
+        
+        return rowHeight
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+        
+        if indexPath.section == 1 && indexPath.row == 0
+        {
+            bIsFromCellExpanded = !bIsFromCellExpanded
+            
+            if bIsFromCellExpanded
+            {
+                bIsToCellExpanded = false
+            }
+            
+            self.tableView.reloadData()
+        }
+        else if indexPath.section == 1 && indexPath.row == 1
+        {
+            bIsToCellExpanded = !bIsToCellExpanded
+            
+            if bIsToCellExpanded
+            {
+                bIsFromCellExpanded = false
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
+
 }
